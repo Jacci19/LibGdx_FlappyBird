1b6f62ef10e3c7e839c4846a8e2480af571f970d
Jacci19 <jacek205@wp.pl>
2019-02-19 10:53:00 +0100

    03_lessons 7; react on bird click

--- core/src/pl/jacci/flappybird/sprites/Bird.java
+++ core/src/pl/jacci/flappybird/sprites/Bird.java
@@ -17,12 +17,24 @@ public class Bird {
     }
 
     public void update(float dt){
-        velocity.add(0, GRAVITY, 0);
+        if (position.y > 0){
+            velocity.add(0, GRAVITY, 0);
+        }
         velocity.scl(dt);
         position.add(0, velocity.y, 0);
+        if (position.y < 0){
+            position.y = 0;
+        }
 
         velocity.scl(1/dt);
     }
+
+    public void jump(){
+        velocity.y = 250;
+    }
+
+
+    //GETTERY
 
     public Vector3 getPosition() {
         return position;
@@ -31,4 +43,5 @@ public class Bird {
     public Texture getTexture() {
         return bird;
     }
+
 }
--- core/src/pl/jacci/flappybird/states/PlayState.java
+++ core/src/pl/jacci/flappybird/states/PlayState.java
@@ -1,5 +1,7 @@
 package pl.jacci.flappybird.states;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import pl.jacci.flappybird.FlappyDemo;
@@ -7,16 +9,20 @@ import pl.jacci.flappybird.sprites.Bird;
 
 public class PlayState extends State {
     private Bird bird;
+    private Texture bg;
 
     public PlayState(GameStateManager gsm) {
         super(gsm);
         bird = new Bird(50, 300);
         cam.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
+        bg = new Texture("bg.png");
     }
 
     @Override
     protected void handleInput() {
-
+        if(Gdx.input.justTouched()){
+            bird.jump();
+        }
     }
 
     @Override
@@ -30,7 +36,10 @@ public class PlayState extends State {
     public void render(SpriteBatch sb) {
         sb.setProjectionMatrix(cam.combined);
         sb.begin();
+        sb.draw(bg, cam.position.x - (cam.viewportWidth / 2), 0);
         sb.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
+//        sb.draw(tube.getTopTube(), tube.getPosTopTube().x, tube.getPosTopTube().y);
+//        sb.draw(tube.getBottomTube(), tube.getPosBotTube().x, tube.getPosBotTube().y);
         sb.end();
     }
 
